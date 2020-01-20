71edaaf28957cbc5c7a87d215122be96331686c5
Min <mmin@xia34536m.active.local>
2020-01-20 17:17:14 +0800

    first

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,27 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+const store = new Vuex.Store({
+  state: { count: 0 },
+  mutations: {
+    inc(state, payload) {
+      state.count += payload.number;
+    },
+    desc(state) {
+      state.count--;
+    }
+  },
+  actions: {
+    addAction({ commit, dispatch }) {
+      setTimeout(function() {
+        commit("inc", { number: 200 }); //直接可以获取到commit方法，不用是context.commit
+        dispatch("textAction", { test: "测试" });
+      }, 0);
+    },
+    textAction(context, obj) {
+      console.log(obj);
+    }
+  },
   modules: {}
 });
+export default store;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ count }}</h1>
+    <h2 v-if="count == 0">没人给你点赞~</h2>
+    <h2 v-else-if="count < 0">被人Diss了 {{ count }}~</h2>
+    <h2 v-else>{{ count }}人给你点赞了哦！</h2>
+    <button @click="increment">+1</button>
+    <button @click="descrment">-1</button>
   </div>
 </template>
+<script>
+// import {mapState} from 'vuex';
+export default {
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+    msg() {
+      if (this.$store.state.count == 0) {
+        return "没有人给你点赞啊!";
+      } else if (this.$store.state.count < 1) {
+        return "被人Diss了啊！！！！！";
+      }
+
+      return this.$store.state.count + "人给你点赞了哦";
+    }
+  },
+  methods: {
+    increment() {
+      this.$store.dispatch("addAction");
+    },
+    descrment() {
+      this.$store.commit("desc");
+    }
+  }
+};
+</script>
